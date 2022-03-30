4ab3c444765e0dc5bab44f43a246358271ca9285
Shivangi Zala <shivangizala12@gmail.com>
2022-03-30 22:16:59 +0530

    ,

--- btpmanagement/settings.py
+++ btpmanagement/settings.py
@@ -139,8 +139,8 @@ USE_TZ = True
 # STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'staticfiles')]
-STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
